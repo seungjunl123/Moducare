c0b60728c59f5114b660c6314c1b3d822bdb2d71
유현진 <a92046503@gmail.com>
2024-10-29 14:01:33 +0900

    Spring Initial

---
+++ moducare-BE/src/main/java/world/moducare/ModucareApplication.java
@@ -0,0 +1,13 @@
+package world.moducare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModucareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModucareApplication.class, args);
+	}
+
+}
