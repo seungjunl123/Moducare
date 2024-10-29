9665a1482c8322ecb5f33fb117735297008b3578
유현진 <a92046503@gmail.com>
2024-10-29 13:11:54 +0900

    Initial Spring project

---
+++ moducare-BE/src/test/java/world/moducare/ModucareApplicationTests.java
@@ -0,0 +1,13 @@
+package world.moducare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModucareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
