b7f3fd7772dbb99088618fdce77215ce9a801d18
유현진 <a92046503@gmail.com>
2024-10-31 16:09:44 +0900

    [BE] feat : challenge 생성 api 구현
    challenge 생성 api 구현 완료

---
+++ moducare-BE/src/main/java/world/moducare/domain/challenge/controller/ChallengeController.java
@@ -0,0 +1,27 @@
+package world.moducare.domain.challenge.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import world.moducare.domain.challenge.dto.ChallengeRequestDto;
+import world.moducare.domain.challenge.service.ChallengeService;
+
+@RestController
+@RequiredArgsConstructor
+//@Tag(name = "챌린지 컨트롤러", description = "챌린지 관련 API")
+@RequestMapping("/challenges")
+public class ChallengeController {
+
+    private final ChallengeService challengeService;
+
+    @PostMapping()
+//    @Operation(summary="챌린지 생성 API")
+    public ResponseEntity<Void> makeChallenge(@RequestBody ChallengeRequestDto requestDto) {
+        challengeService.saveChallenge(requestDto);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
