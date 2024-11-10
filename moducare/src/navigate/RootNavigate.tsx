5a36b82719814884a5ab3d5b6ab86bd61b82754f
JinY <wlsdud6221@naver.com>
2024-11-10 15:28:53 +0900

    [FE] fix : 로그인 유지 및 스플래시 로직 추가

--- moducare/src/navigate/RootNavigate.tsx
+++ moducare/src/navigate/RootNavigate.tsx
@@ -1,11 +1,25 @@
 import AuthStackNavigate from './AuthStackNavigate';
 import StackNavigate from './StackNavigate';
-import React from 'react';
+import React, {useEffect} from 'react';
 import useAuthStore from '../store/useAuthStore';
+import useAuth from '../hook/useAuth';
+import SplashScreen from 'react-native-splash-screen';
 const RootNavigate = () => {
-  const {isLoggedIn} = useAuthStore(state => state as {isLoggedIn: boolean});
+  // const {isLoggedIn} = useAuthStore(state => state as {isLoggedIn: boolean});
 
-  return <>{isLoggedIn ? <StackNavigate /> : <AuthStackNavigate />}</>;
+  // return <>{isLoggedIn ? <StackNavigate /> : <AuthStackNavigate />}</>;
+
+  const {isLogin, isLoginLoading} = useAuth();
+
+  useEffect(() => {
+    if (!isLoginLoading) {
+      setTimeout(() => {
+        SplashScreen.hide();
+      }, 500);
+    }
+  }, [isLoginLoading]);
+
+  return <>{isLogin ? <StackNavigate /> : <AuthStackNavigate />}</>;
 };
 
 export default RootNavigate;
