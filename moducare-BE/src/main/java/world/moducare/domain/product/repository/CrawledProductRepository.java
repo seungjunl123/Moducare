a3fb3320e26fc5c96859cb2bd62288ad00627e1b
minji <minipeach0923@gmail.com>
2024-11-14 20:55:04 +0900

    [BE] test: elasticsearch test
    일반 sql과 엘라스틱서치 비교 테스트 완료

--- moducare-BE/src/main/java/world/moducare/domain/product/repository/CrawledProductRepository.java
+++ moducare-BE/src/main/java/world/moducare/domain/product/repository/CrawledProductRepository.java
@@ -1,5 +1,6 @@
 package world.moducare.domain.product.repository;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import world.moducare.domain.product.entity.CrawledProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,12 +9,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CrawledProductRepository extends JpaRepository<CrawledProduct, Long> {
-    @Query("SELECT p FROM CrawledProduct p " +
-            "WHERE p.세부제품특징 LIKE %:keyword% " +
-            "OR p.주요제품특징 LIKE %:keyword% " +
-            "OR p.헤어타입 LIKE %:keyword% " +
-            "OR p.두피타입 LIKE %:keyword% " +
-            "OR p.모발타입 LIKE %:keyword%")
-    List<CrawledProduct> findByKeywordInFeatures(String keyword);
+public interface CrawledProductRepository extends JpaRepository<CrawledProduct, Long>, JpaSpecificationExecutor<CrawledProduct> {
+
 }
