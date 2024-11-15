789ba499b4ab17da5d02b481798d35c013c743d6
유현진 <a92046503@gmail.com>
2024-11-15 10:19:38 +0900

    [BE]hotfix : firebaseConfig 코드 변경
    코드 변경 후 에러 확인 예정

--- moducare-BE/src/main/java/world/moducare/global/config/firebase/FirebaseConfig.java
+++ moducare-BE/src/main/java/world/moducare/global/config/firebase/FirebaseConfig.java
@@ -5,23 +5,30 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${firebase.config.path}")
+    private String firebaseConfigPath;
     private FirebaseApp firebaseApp;
 
     @PostConstruct
     public FirebaseApp initializeFcm() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
         firebaseApp = FirebaseApp.initializeApp(options);
         return firebaseApp;
     }
+
     @Bean
     public FirebaseMessaging initFirebaseMessaging() {
         return FirebaseMessaging.getInstance(firebaseApp);
